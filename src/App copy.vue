0e4e1cbf1d4f3239517a99138e9e56bccd30ed32
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-25 15:54:29 +0100

    Surveiller des Objets Réactifs
    Surveiller des Objets Réactifs

---
+++ src/App copy.vue
@@ -0,0 +1,29 @@
+<template>
+  <fieldset>
+    <div>
+      <p>Valeur : {{ data }}</p>
+      
+      <button @click="increment">Incrémenter</button>
+    </div>
+  </fieldset>
+  </template>
+  
+  <script setup>
+  import {
+    ref,
+    watch,
+    computed
+  } from 'vue';
+  
+  const data = ref(0);
+  const computedValue = computed(() => data.value * 2);
+ 
+  watch(data, (newValue, oldValue) => {
+    console.log(`La valeur de data est passée de ${oldValue} à ${newValue}`);
+    
+  }, {immediate: true});
+  const increment = () => {
+    data.value++;
+  };
+  </script>
+  
---
+++ src/App1.vue
@@ -0,0 +1,27 @@
+<template>
+<fieldset>
+  <div>
+    <p>Valeur : {{ data }}</p>
+    <p>la valeur calculéé  {{ computedValue }}</p>
+    <button @click="increment">Incrémenter</button>
+  </div>
+</fieldset>
+</template>
+
+<script setup>
+import {
+  ref,
+  watch,
+  computed
+} from 'vue';
+
+const data = ref(0);
+const computedValue = computed(() => data.value * 2);
+watch(computedValue, (newValue, oldValue) => {
+  console.log(`La valeur de data est passée de ${oldValue} à ${newValue}`);
+  console.log(`La valeur calculée est passée de ${oldValue} à ${newValue} `);
+});
+const increment = () => {
+  data.value++;
+};
+</script>
