368e5d70abd5906dadbf9d1afde8e0e27315a5e1
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-25 09:48:59 +0100

    v-model
    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
-
-
- <template>
-  <div class=" grid gap-4 justify-center items-center"> 
-          <h1 class="text-7xl text-red-600 font-bold underline">
-        Count 
-        </h1>
-        <button @click="increment" class="mt-5 px-7 py-4 bg-green-500 rounded-md text-3xl text-white" > increment = {{ count }}</button>
-  </div>
+<template>
+ <input type="text" v-model="name" />
 </template>
 
 
   <script setup>
-  import { ref } from 'vue'
-  const count = ref(0);
-
-  const increment = () => {
-    count.value++;
-  }
-  
-  </script>.gitattribute
+  import { ref } from 'vue';
+  const name = ref('');
+  </script>
